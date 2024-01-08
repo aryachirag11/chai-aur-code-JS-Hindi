9d2fbdd415e8a294623c3bb6de980bc20eb36559
Chirag Arya <88162504+aryachirag11@users.noreply.github.com>
2024-01-08 14:36:48 +0000

    number-and-maths.js

---
+++ _01_JS/08_Numbers-JS.js
@@ -0,0 +1,37 @@
+// // Numbers are a primitive datatype in JS
+
+
+// let score = 100;
+// const userId = new Number(1180056);
+// console.log(userId);
+
+// console.log(userId.toString());
+
+// let PI = 3.14159265359;
+// console.log(PI.toFixed(4)); // round off to 4 digits after decimal
+
+
+// console.log(userId.toPrecision(3));
+
+// console.log(userId.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+
+
+//************** MATHS ************************/
+let arr = [1,24,5,678,78];
+ console.log(Math.abs(-98));
+ console.log(Math.round(99.7898).toFixed(2));
+ console.log(Math.ceil(99.1));
+ console.log(Math.floor(99.9));
+ console.log(Math.pow(2, 5));
+ console.log(Math.sqrt(98).toFixed(2));
+ console.log((Math.random()*100).toFixed(0));
+ console.log(Math.max(1,24,5,678,78));
+ console.log(Math.min(1,24,5,678,78));
+
+ let min = 10;
+ let max = 20;
+
+ //console.log(Math.floor(Math.random() * (max - min + 1)) + min);
